18472759a2f4073ec643ad855eac41145cda5f72
yuantaoBai <yuantaob@umich.edu>
2023-03-28 02:24:08 -0400

    maping finish v1.0

--- mbot/mbot_autonomy/include/slam/mapping.hpp
+++ mbot/mbot_autonomy/include/slam/mapping.hpp
@@ -17,11 +17,11 @@ double REVERSE_MODEL_ALPHA = 0.01;
 // angle of approximation
 double REVERSE_MODEL_BETA = 2*3.1415926/100;  
 
-double L_FREE = -1.986;   // ln(p_occupied/p_free)  = log(0.25/0.75)
+int L_FREE = -2;   // ln(p_occupied/p_free)  = log(0.25/0.75)
 
-double L_OCCUPIED = 1.986;   // ln(p_occupied/p_free)  = log(.75/0.25)
+int L_OCCUPIED = 2;   // ln(p_occupied/p_free)  = log(.75/0.25)
 
-double L_0 = 0;   // log(prior)
+int L_0 = 0;   // log(prior), typically 0
 
 // 
 /**
@@ -49,6 +49,16 @@ public:
     */
     void updateMap(const mbot_lcm_msgs::lidar_t& scan, const mbot_lcm_msgs::pose_xyt_t& pose, OccupancyGrid& map);
 
+
+    /**                                                                                     *
+    * crude inverse senfor model , see https://youtu.be/1f_m5aJFIj4  lecture 12
+    * 
+    * \param    ray             adjusted_ray_t, single laser ray
+    * \param    map             OccupancyGrid, grid map which we need to update
+    * \return   vector, containing inververse sensor model value of each cell
+    */
+    std::vector<int> crudeInverseSensorModel(const adjusted_ray_t& ray, OccupancyGrid& map);
+
 private:
     
     const float  kMaxLaserDistance_;
@@ -61,7 +71,7 @@ private:
     void scoreRay(const adjusted_ray_t& ray, OccupancyGrid& map);
     std::vector<Point<int>> bresenham(const adjusted_ray_t& ray, const OccupancyGrid& map);
     std::vector<Point<int>> divideAndStepAlongRay(const adjusted_ray_t& ray, const OccupancyGrid& map);
-    std::vector<Point<int>> crudeReverseSensorModel(const adjusted_ray_t& ray, std::vector<Point<int>> cells);
+    
     //////////////////// TODO: Add any private members needed for your occupancy grid mapping algorithm ///////////////
 
 };
